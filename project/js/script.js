5c88a35454510f21d7b45283ee38b8867106c1c1
atweber <atpbroth@gmail.com>
2019-02-19 13:41:51 +0400

    add cart's open/close funcs

--- project/js/script.js
+++ project/js/script.js
@@ -1,11 +1,25 @@
 const cartWrapper = document.querySelector('.cart__wrapper'),
-    cart = document.querySelector('.cart'),
-    close = document.querySelector('.cart__close'),
-    open = document.querySelector('#cart'),
-    goodsBtn = document.querySelector('.goods__btn'),
-    products = document.querySelector('goods__item'),
-    confirm = document.querySelector('.confirm'),
-    badge = document.querySelector('.nav__badge'),
-    totalCost = document.querySelector('.cart__total > span'),
-    titles = document.querySelector('.goods__title');
+      cart = document.querySelector('.cart'),
+      close = document.querySelector('.cart__close'),
+      open = document.querySelector('#cart'),
+      goodsBtn = document.querySelector('.goods__btn'),
+      products = document.querySelector('goods__item'),
+      confirm = document.querySelector('.confirm'),
+      badge = document.querySelector('.nav__badge'),
+      totalCost = document.querySelector('.cart__total > span'),
+      titles = document.querySelector('.goods__title');
 
+function openCart()
+{
+    cart.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+}
+
+function closeCart()
+{
+    cart.style.display = 'none';
+    document.body.style.overflow = '';
+}
+
+open.addEventListener('click', openCart);
+close.addEventListener('click', closeCart);

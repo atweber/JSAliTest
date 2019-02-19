4aae72c6a5b55d5e9ad111bea8ea0e2b9023d30e
atweber <atpbroth@gmail.com>
2019-02-19 15:19:24 +0400

    add items to cart

--- project/js/script.js
+++ project/js/script.js
@@ -2,12 +2,12 @@ const cartWrapper = document.querySelector('.cart__wrapper'),
       cart = document.querySelector('.cart'),
       close = document.querySelector('.cart__close'),
       open = document.querySelector('#cart'),
-      goodsBtn = document.querySelector('.goods__btn'),
-      products = document.querySelector('goods__item'),
+      goodsBtn = document.querySelectorAll('.goods__btn'),
+      products = document.querySelectorAll('.goods__item'),
       confirm = document.querySelector('.confirm'),
       badge = document.querySelector('.nav__badge'),
       totalCost = document.querySelector('.cart__total > span'),
-      titles = document.querySelector('.goods__title');
+      titles = document.querySelectorAll('.goods__title');
 
 function openCart()
 {
@@ -23,3 +23,26 @@ function closeCart()
 
 open.addEventListener('click', openCart);
 close.addEventListener('click', closeCart);
+
+goodsBtn.forEach(function (btn, i)
+{
+    btn.addEventListener('click', () => 
+    {
+        let  item = products[i].cloneNode(true),
+             trigger = item.querySelector('button'),
+             removeBtn = document.createElement('div'),
+             empty = cartWrapper.querySelector('.empty');
+
+        trigger.remove();
+
+        removeBtn.classList.add('goods__item-remove');
+        removeBtn.innerHTML = '&times';
+        item.appendChild(removeBtn);
+
+        cartWrapper.appendChild(item);
+        if(empty)
+        {
+            empty.remove();
+        }
+    });
+});
